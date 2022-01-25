3c79479babf27128fba9ba35a0b7259c477c686e
Herman Kudria <h.kudrya@hotmail.com>
2022-01-25 21:40:58 +0100

    Interface

--- tebEducation/9_oop/ex/8_interface.php
+++ tebEducation/9_oop/ex/8_interface.php
@@ -12,49 +12,97 @@
 -->
 <?php
 
-abstract class GeometricFigure
+interface Calculation
 {
-    public static $count=0;
+    public function area() : float;
+    public function circuit() : float;
+}
+
+abstract class GeometricFigure implements Calculation
+{
+    private static int $count=0;
+    private static int $countDestroy=0;
     public function __construct()
     {
-        GeometricFigure::$count++;
+        self::$count++;
+        echo 'Created object of class : '. get_class($this). ' Was all created: ' . self::$count .'<br>';
     }
-
+    public static function getCount(): int
+    {
+        return self::$count;
+    }
     public function __destruct()
     {
-       echo 'Kill ' . get_class($this) . '<br>';
+        self::$countDestroy++;
+        echo 'Destroyed object of class : '. get_class($this). ' Was all destroyed: ' . self::$countDestroy.'<br>' ;
     }
 
-    public abstract function area();
-    public abstract function perimetr();
-    public abstract function draw();
+
+    public function draw()
+    {
+        echo 'draw '.get_class($this). '<br>';
+    }
 }
 
 class Circle extends GeometricFigure
 {
-    public function area()
+    protected float $radius;
+
+    public function __construct($radius)
     {
-        echo 'area '.get_class($this). '<br>';
+        parent::__construct();
+        $this->radius = $radius;
     }
 
-    public function perimetr()
+    public function area() : float
     {
-        echo 'square '.get_class($this) . '<br>';
+       return pow($this->radius,2) * pi();
     }
 
-    public function draw()
+    public function circuit() : float
     {
-       echo 'draw '.get_class($this). '<br>';
+        return $this->radius * pi() * 2;
+    }
+
+}
+
+class Rectangle extends GeometricFigure
+{
+    protected float $width, $height;
+
+    public function __construct($width,$height)
+    {
+        parent::__construct();
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() : float
+    {
+        return $this->width * $this->height;
+    }
+
+    public function circuit() : float
+    {
+        return ($this->width + $this->height)*2;
     }
 
 }
 
 
-$obj = new Circle();
-$obj1 = new Circle();
-$obj2 = new Circle();
-echo 'How many figure was created ' . GeometricFigure::$count . '<br>';
-unset($obj);
+$obj = new Circle(5);
+$obj1 = new Circle(4);
+$obj2 = new Circle(3);
+$obj3 = new Rectangle(2,5);
+unset($obj); //kill object
+echo 'How many figure was created ' . GeometricFigure::getCount() . '<br>';
+echo sprintf('Circuit: %.2f <br>',$obj1->circuit()) ; //we can use function round($variable,num) or number_format($variable,num)
+echo sprintf('Area: %.2f <br>',$obj1->area()) ;
+echo round($obj1->circuit(),2) . 'cm<br>';
+echo number_format($obj1->area(),2,',').'cm<br>';
+echo 'Rectangle: ';
+echo $obj3->area(). 'cm | ';
+echo $obj3->circuit(). 'cm<sup>2</sup><br>';
 
 
 
